60d1d47a5927c7a013816be57e258d50a46ba47e
Israeluiux <israelbalogun15@gmail.com>
2025-08-11 19:40:06 +0100

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/Portfolio" element={ <Home /> } />
+        <Route path="/" element={ <Home /> } />
         <Route path="/contact" element={ <Contact /> } />
         <Route path="/about" element={ <About /> } />
         <Route path="/myworks" element={ <Works /> } />
