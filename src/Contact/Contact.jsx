9449a916ea372bc63937358778186a0c38f6d657
Israeluiux <israelbalogun15@gmail.com>
2025-08-14 02:41:54 +0100

    re-pushing

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,11 +1,9 @@
 import "./Contact.css"
-import Form from "../Components/Forms/Form.jsx"
 import Formv2 from "../Components/Forms/Formv2.jsx"
 
 const Contact = () => {
     return (
         <div>
-          {/* <Form /> */}
           <Formv2 />
         </div>
     )
